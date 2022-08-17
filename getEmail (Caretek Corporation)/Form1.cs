ce6e278484ee2e0d414c38170e5a7aa066ce161d
Naeem Malik <naaeemmalik21@gmail.com>
2022-08-17 05:58:39 +0500

    first commit

---
+++ getEmail (Caretek Corporation)/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+using MailKit.Net.Pop3;
+using MailKit;
+using MimeKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using System.IO;
+
+namespace getEmail__Caretek_Corporation_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+             * check email where subject starts with PatientReport_
+             Name: [Some Random Text of length between 3- 20] space [Some Random Text of length between 3- 20].   
+            Age: Some Random number between 20 and 100. 
+            file name: MMDDYYYY_HHMMSS.txt
+             For example 
+            Name:   Rene Davis 
+            Age: 54. 
+            */
+//get new mails using MailKit
+            var mailRepository = new MailRepository("imap.gmail.com", 993, true, "gointoall@gmail.com", "pass");
+           mailRepository.GetAllMails();
+            //exit
+            this.Close();
+
+        }
+
+      
+    }
+public class MailRepository : Form
+{
+    private readonly string mailServer, login, password;
+    private readonly int port;
+    private readonly bool ssl;
+
+    public MailRepository(string mailServer, int port, bool ssl, string login, string password)
+    {
+        this.mailServer = mailServer;
+        this.port = port;
+        this.ssl = ssl;
+        this.login = login;
+        this.password = password;
+    }
+
+
+
+    public void GetAllMails()
+    {
+
+        using (var client = new ImapClient())
+        {
+            client.Connect(mailServer, port, ssl);
+
+            // Note: since we don't have an OAuth2 token, disable
+            // the XOAUTH2 authentication mechanism.
+            client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+            client.Authenticate(login, password);
+
+            // The Inbox folder is always available on all IMAP servers...
+            var inbox = client.Inbox;
+            inbox.Open(FolderAccess.ReadOnly);
+            var results = inbox.Search(SearchOptions.All, SearchQuery.DeliveredAfter(DateTime.Now.AddMinutes(-10) ));
+            foreach (var uniqueId in results.UniqueIds)
+            {
+                var msg = inbox.GetMessage(uniqueId);
+                    if (msg.Subject.IndexOf("PatientReport_")==0 && msg.Subject.EndsWith(".txt") && msg.Subject.Length==33)
+                    {
+                        var at= msg.Attachments.FirstOrDefault();
+                        if (at is MimePart part && part.FileName.EndsWith(".txt", StringComparison.OrdinalIgnoreCase))
+                        {
+                            using (var stream = new MemoryStream())
+                            {
+                                part.Content.DecodeTo(stream);
+                                stream.Position = 0;
+                                using (var reader = new StreamReader(stream))
+                                {
+                                    //first line is Name
+                                    var name = reader.ReadLine();
+                                    //second line is Age
+                                    var age = reader.ReadLine();
+                                    //remove Name: from name
+                                    name = name.Substring(6);
+                                    //remove Age: from age
+                                    age = age.Substring(4);
+                                    MessageBox.Show( name + "\n" + age, msg.Subject );
+                                }
+                            }
+                        }
+
+                    }
+                }
+
+                client.Disconnect(true);
+        }
+
+    }
+}
+
+}
---
+++ sendEmail (Caretek Corporation)/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sendEmail__Caretek_Corporation_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+            The system will generate a new file using Memory Stream with name format "MMDDYYYY_HHMMSS.txt". For example the file name will be "01232022_153524.txt". The file should be created in memory only using Memory Stream and should not be saved on hard drive. 
+            Then System will put some random text in that file in the following way 
+            Name: [Some Random Text of length between 3- 20] space [Some Random Text of length between 3- 20].   
+            Age: Some Random number between 20 and 100. 
+             For example 
+            Name:   Rene Davis 
+            Age: 54. 
+
+            Then send it through Gmail with following subject 
+
+            PatientReport_ + file name 
+
+            For example subject will be “PatientReport_01232022_153524.txt" 
+            */
+
+            //made by Muhammad Naeem Tariq
+
+            //creating memory stream
+            string fileName = DateTime.Now.ToString("MMddyyyy_HHmmss") + ".txt";
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            System.IO.StreamWriter writer = new System.IO.StreamWriter(ms);
+            string fileContent = "Name: " + RandomString(3, 20) + " " + RandomString(3, 20) + "\n" + "Age: " + RandomNumber(20, 100);
+            writer.Write(fileContent);
+            writer.Flush();
+            ms.Seek(0, System.IO.SeekOrigin.Begin);
+
+
+            //sending email
+            System.Net.Mime.ContentType ct = new System.Net.Mime.ContentType(System.Net.Mime.MediaTypeNames.Text.Plain);
+            System.Net.Mail.Attachment attach = new System.Net.Mail.Attachment(ms, ct);
+            attach.ContentDisposition.FileName = fileName;
+            attach.ContentDisposition.DispositionType = System.Net.Mime.DispositionTypeNames.Attachment;
+            System.Net.Mail.MailMessage msg = new System.Net.Mail.MailMessage();
+            msg.From = new System.Net.Mail.MailAddress("gointoall@gmail.com");
+            msg.To.Add(new System.Net.Mail.MailAddress("gointoall@gmail.com"));
+            msg.Subject = "PatientReport_" + fileName;
+            msg.Body = "";
+            msg.Attachments.Add(attach);
+            System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient("smtp.gmail.com");
+            smtp.Port = 587;
+            smtp.Credentials = new System.Net.NetworkCredential("gointoall@gmail.com", "pass");
+            smtp.EnableSsl = true;
+            smtp.Send(msg);
+
+            MessageBox.Show("Email has been sent");
+            this.Close();
+
+        }
+        Random random = new Random();
+        public string RandomString(int length, int max)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return new string(Enumerable.Repeat(chars, RandomNumber(length, max))
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        public static int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+    }
+}
+
